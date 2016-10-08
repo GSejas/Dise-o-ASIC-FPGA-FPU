fbb307ffb79b0010ba2680fd7ca7c87dc7560db5
Jsequeira <jsequeira03@gmail.com>
2016-10-08 16:10:19 -0600

    creacion de file_list

--- ASIC_FLOW/script_sources_softlink.sh
+++ ASIC_FLOW/script_sources_softlink.sh
@@ -10,7 +10,8 @@
 dir_list=(ASIC_cordic_Arch2 ASIC_cordic_Arch3 ASIC_fpaddsub_arch2 ASIC_fpaddsub_arch3 ASIC_fpmult_arch2 ASIC_fpmult_arch3 ASIC_KOA ASIC_RKOA)
 for i in "${dir_list[@]}"
 do
-  ln --symbolic ../my_sourcefiles/Source_Files/FPU_Interface/${i:5}/*.v $i/integracion_fisica/front_end/source/.
+  rm $i/integracion_fisica/front_end/source/FPU_Interface
+  ln --symbolic $PWD/../my_sourcefiles/Source_Files/FPU_Interface/${i:5}/*.v $i/integracion_fisica/front_end/source/.
 done
 
 #find $PWD -type d -name "source" -exec cp -Rp temp '{}' \;
