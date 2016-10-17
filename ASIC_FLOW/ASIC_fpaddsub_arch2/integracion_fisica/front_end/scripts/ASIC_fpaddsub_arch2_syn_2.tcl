f38544af4a6eb0b9c880a4d0a13deffd3d807011
Jsequeira <jsequeira03@gmail.com>
2016-10-17 13:56:05 -0600

    some scripts for bottom up

--- ASIC_FLOW/ASIC_fpaddsub_arch2/integracion_fisica/front_end/scripts/ASIC_fpaddsub_arch2_syn_2.tcl
+++ ASIC_FLOW/ASIC_fpaddsub_arch2/integracion_fisica/front_end/scripts/ASIC_fpaddsub_arch2_syn_2.tcl
@@ -14,11 +14,12 @@ set PREC_PARAM(1) "W=64,SW=52,EW=11,SWR=55,EWR=6";
 # Eliminar diseños previos
 set DESIGN_NAME  "fpaddsub_arch2"
 set TOP_NAME     "FPU_Add_Subtract_Function"
-#set SUBMODULE_NAME     "FPU_Add_Subtract_Function"
 set CONTRAINTS_FILE_NAME "ASIC_fpaddsub_arch2_syn_constraints.tcl"
+
+set SUBMODULE_NAME {"Tenth_Phase"     "Oper_Start_In" "LZD"          "FSM_Add_Subtract" "Exp_Operation"       "Barrel_Shifter" "Add_Subt"}
+
 remove_design -designs
-#set SUBMODULE_NAME {"Tenth_Phase"     "Oper_Start_In" "LZD"          "FSM_Add_Subtract" "Exp_Operation"       "Barrel_Shifter" "Add_Subt"}
-#set ParamSubm      {"W=32,SW=23,EW=8" "W=32"          "SWR=26,EWR=5" ""                 "EW=8"                "SWR=26,EWR=5"   "SWR=26"}
+
 #WE PARSE THE FILE_LIST GENERATED OUTSIDE THIS SCRIPT LINK:http://wiki.tcl.tk/367
 set fp [open "scripts/file_list" r]
 set file_sources [read $fp]
@@ -33,18 +34,19 @@ foreach line $data {
 }
 
 set x 0;
-
 while {$x < 2} {
 
 #Elaboramos el módulo principal
-elaborate "$module" -parameters "$PREC_PARAM($x)" -architecture verilog -library WORK
+elaborate $TOP_NAME -parameters "$PREC_PARAM($x)" -architecture verilog -library WORK -ref
 
 #Enlazar los demás módulos al módulo principal
+uniquify
+
 link
 
 #Escribir el archivo *.ddc (base de datos sin sintetizar)
 write -hierarchy -format ddc -output \
-./db/$PRECISION($x)/$module\_syn_unmapped.ddc
+./db/$PRECISION($x)/$DESIGN_NAME\_syn_unmapped.ddc
 
 #Aplicar especificaciones de diseño (constraints)
 source $CONTRAINTS_FILE_NAME
@@ -54,42 +56,43 @@ propagate_constraints
 check_design
 
 #Compilar el diseño
-compile_ultra
+compile
 
 #Escribir la lista de nodos a nivel de compuertas (Gate Level Netlist) que se utiliza para:
 #- Verificar el funcionamiento lógico del sistema digital después de la Síntesis RTL.
 #- Como una de las entradas para el sintetizador físico (IC Compiler).
 set verilogout_no_tri true
-change_names -hierarchy -rules verilog
+#change_names -hierarchy -rules verilog
+
 write -hierarchy -format verilog -output \
-./db/$PRECISION($x)/$module\_syn.v
+./db/$PRECISION($x)/$DESIGN_NAME\_syn.v
+
+  foreach module $SUBMODULE_NAME
+  {
+    write -hierarchy -format ddc -output \
+    ./db/$PRECISION($x)/$module\_syn_unmapped.ddc
+  }
 
 #Generar los reportes
 
-report_power -analysis_effort high > reports/$PRECISION($x)/$module\_syn_power.txt
-report_area >   reports/$PRECISION($x)/$module\_syn_area.txt
-report_cell >   reports/$PRECISION($x)/$module\_syn_cell.txt
-report_qor >    reports/$PRECISION($x)/$module\_syn_qor.txt
-report_timing > reports/$PRECISION($x)/$module\_syn_timing.txt
-report_port >   reports/$PRECISION($x)/$module\_syn_port.txt
+report_power -analysis_effort high > reports/$DESIGN_NAME\_$PRECISION($x)_syn_power.txt
+report_area >   reports/$PRECISION($x)/$DESIGN_NAME\_syn_area.txt
+report_cell >   reports/$PRECISION($x)/$DESIGN_NAME\_syn_cell.txt
+report_qor >    reports/$PRECISION($x)/$DESIGN_NAME\_syn_qor.txt
+report_timing > reports/$PRECISION($x)/$DESIGN_NAME\_syn_timing.txt
+report_port >   reports/$PRECISION($x)/$DESIGN_NAME\_syn_port.txt
 
 #Escribir el archivo *.ddc (base de datos sintetizada)
 write -hierarchy -format ddc -output \
-./db/$PRECISION($x)/$module\_syn_mapped.ddc
+./db/$PRECISION($x)/$DESIGN_NAME\_syn_mapped.ddc
 
 #Escribir el archivo *.sdc (Synopsys Design Constraints), utilizado como una de las entradas
 #para el sintetizador físico (IC Compiler)
-write_sdc ./db/$PRECISION($x)/$module\_syn.sdc
+write_sdc ./db/$PRECISION($x)/$DESIGN_NAME\_syn.sdc
 
 #Revisar la configuración de temporizado
 check_timing
 
 #FINALIZAMOS EL LOOP
 set x [expr {$x + 1}]
-  foreach module $SUBMODULE_NAME
-  {
-
-  }
-
-
 }
