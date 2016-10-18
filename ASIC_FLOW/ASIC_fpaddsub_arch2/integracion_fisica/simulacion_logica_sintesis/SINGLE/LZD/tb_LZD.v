c16b1e3500f46889faa8ad96722c2ea29f35ba7d
Jsequeira <jsequeira03@gmail.com>
2016-10-17 20:13:05 -0600

    tested on Vivado new LZD with CASE statement

--- ASIC_FLOW/ASIC_fpaddsub_arch2/integracion_fisica/simulacion_logica_sintesis/SINGLE/LZD/tb_LZD.v
+++ ASIC_FLOW/ASIC_fpaddsub_arch2/integracion_fisica/simulacion_logica_sintesis/SINGLE/LZD/tb_LZD.v
@@ -22,34 +22,48 @@ module tb_LZD (); /* this is automatically generated */
   end
 
   // (*NOTE*) replace reset, clock
-
+`ifdef SINGLE
   parameter SWR = 26;
   parameter EWR = 5;
-
+`endif
+`ifdef DOUBLE
+  parameter SWR = 55;
+  parameter EWR = 6;
+`endif
 
   reg           load_i;
   reg [SWR-1:0] Add_subt_result_i;
   wire [EWR-1:0] Shift_Value_o;
 
-  LZD inst_LZD (
+  LZD #(
+      .SWR(SWR),
+      .EWR(EWR)
+    ) inst_LZD (
       .clk               (clk),
       .rst               (rst),
       .load_i            (load_i),
       .Add_subt_result_i (Add_subt_result_i),
       .Shift_Value_o     (Shift_Value_o)
     );
-   integer Contador = 0;
+   integer Contador = 1;
+   integer incremental = 1;
    always begin
              #(5*PERIOD/2) Contador = Contador + 1;
              Add_subt_result_i = Contador;
              #(5*PERIOD/2);
+            //incremental = incremental + incremental ;
+
    end
 
   initial begin
-    load_i = 1
-    repeat(1024)@(posedge clk);
-    $finish;
-  end
+    load_i = 1;
+    @(posedge clk);
+      begin
+        if (Contador==(2**EWR)) begin
+          $finish;
+        end
+      end
+    end
 
 
 endmodule
