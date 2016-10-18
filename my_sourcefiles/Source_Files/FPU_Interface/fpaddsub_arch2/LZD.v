49cbc57ccd016200cbd08ab44d751fd495d49f9c
Jsequeira <jsequeira03@gmail.com>
2016-10-17 20:47:01 -0600

    added some embedded script inside the LZD

--- my_sourcefiles/Source_Files/FPU_Interface/fpaddsub_arch2/LZD.v
+++ my_sourcefiles/Source_Files/FPU_Interface/fpaddsub_arch2/LZD.v
@@ -18,6 +18,26 @@
 // Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
+//dc_script_begin
+// set LIB_NAME scx3_cmos8rf_lpvt_tt_1p2v_25c.db
+
+// set CLK_PERIOD 10
+// set CLK_NAME clk
+// set INPUT_DELAY_MAX 3
+// #set the fastest input data arrival time to check for hold time changes to 1000pS
+// set INPUT_DELAY_MIN 2
+// # set the setup time requirements for the next block to 2nS
+// set OUTPUT_DELAY_MAX 3
+// # set the hold time requirements for the next block to 150pS
+// set OUTPUT_DELAY_MIN 1
+// set CLK_TRANSITION 1
+// set_input_delay -max $INPUT_DELAY_MAX -clock $CLK_NAME $ALL_IN_EX_CLK_NAME
+// set_input_delay -min $INPUT_DELAY_MIN -clock $CLK_NAME $ALL_IN_EX_CLK_NAME
+// #Configuración del retardo en las señales de salida                -> page 5-26, 7-18
+// set_output_delay -max $OUTPUT_DELAY_MAX -clock $CLK_NAME $ALL_OUT_NAME
+// set_output_delay -min $OUTPUT_DELAY_MIN -clock $CLK_NAME $ALL_OUT_NAME
+//set_clock_transition $CLK_TRANSITION [get_clocks $CLK_NAME]
+//dc_script_end
 
 
 module LZD#(parameter SWR=26, parameter EWR=5)(
