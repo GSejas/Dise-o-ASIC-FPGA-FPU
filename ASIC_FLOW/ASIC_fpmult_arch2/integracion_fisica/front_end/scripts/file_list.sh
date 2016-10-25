30e6183d0f01197c7855362c2f432c248e0214c8
Jsequeira <jsequeira03@gmail.com>
2016-10-25 17:02:03 -0600

    some files
    More from the structure.

---
+++ ASIC_FLOW/ASIC_fpmult_arch2/integracion_fisica/front_end/scripts/file_list.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+find "../../front_end/source" -name "*.v" >> file_list
+#!/bin/bash
+find "../../front_end/source" -name "*.v" >> file_list
--- ASIC_FLOW/script_create_single-double.sh
+++ ASIC_FLOW/script_create_single-double.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dir_list=($1)
+dir_list=(ASIC_fpmult_arch2 ASIC_fpmult_arch3)
 for i in "${dir_list[@]}"
 do
 #CREATE DC STRUCTURE
@@ -17,6 +17,7 @@ do
   mkdir $i/integracion_fisica/simulacion_logica_sintesis
   mkdir $i/integracion_fisica/simulacion_logica_sintesis/SINGLE
   mkdir $i/integracion_fisica/simulacion_logica_sintesis/DOUBLE
+
   mkdir $i/integracion_fisica/simulacion_logica_behavioral
   mkdir $i/integracion_fisica/simulacion_logica_behavioral/SINGLE
   mkdir $i/integracion_fisica/simulacion_logica_behavioral/DOUBLE
@@ -25,11 +26,30 @@ do
   cp ibm13rflpvt.v $i/integracion_fisica/simulacion_logica_sintesis/SINGLE
   cp ibm13rflpvt.v $i/integracion_fisica/simulacion_logica_sintesis/DOUBLE
 
+  cp filelist4sim.sh $i/integracion_fisica/simulacion_logica_behavioral/SINGLE
+  cp filelist4sim.sh $i/integracion_fisica/simulacion_logica_behavioral/DOUBLE
+
+  cp get_saif.sh $i/integracion_fisica/simulacion_logica_behavioral/SINGLE
+  cp get_saif.sh $i/integracion_fisica/simulacion_logica_behavioral/DOUBLE
+  cp get_saif.sh $i/integracion_fisica/simulacion_logica_sintesis/SINGLE
+  cp get_saif.sh $i/integracion_fisica/simulacion_logica_sintesis/DOUBLE
+
+  cp run_sim.sh $i/integracion_fisica/simulacion_logica_behavioral/SINGLE
+  cp run_sim.sh $i/integracion_fisica/simulacion_logica_behavioral/DOUBLE
+  cp run_sim.sh $i/integracion_fisica/simulacion_logica_sintesis/SINGLE
+  cp run_sim.sh $i/integracion_fisica/simulacion_logica_sintesis/DOUBLE
+
+  touch $i/integracion_fisica/front_end/scripts/file_list.sh
+  chmod 777 $i/integracion_fisica/front_end/scripts/file_list.sh
+  echo "#!/bin/bash"    >> $i/integracion_fisica/front_end/scripts/file_list.sh
+  echo "find \"../../front_end/source\" -name \"*.v\" >> file_list" >> $i/integracion_fisica/front_end/scripts/file_list.sh
+
 #WE COPY SOME TEMPLATE SCRIPTS
   cp ASIC_fpaddsub_arch3_syn.tcl $i/integracion_fisica/front_end/scripts
   mv "$i/integracion_fisica/front_end/scripts/ASIC_fpaddsub_arch3_syn.tcl" "${i}/integracion_fisica/front_end/scripts/${i}_syn.tcl"
   cp ASIC_fpaddsub_arch3_syn_constraints.tcl $i/integracion_fisica/front_end/scripts
   mv "$i/integracion_fisica/front_end/scripts/ASIC_fpaddsub_arch3_syn_constraints.tcl" "${i}/integracion_fisica/front_end/scripts/${i}_syn_constraints.tcl"
+
   cp .synopsys_dc.setup $i/integracion_fisica/front_end
 
 done
